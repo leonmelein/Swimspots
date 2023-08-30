e0763cc857139436711e63ada2ef73ebbf854a59
Léon Melein <leon@thesociallions.nl>
2023-08-30 09:55:06 +0200

    GeoIP bbox zoom

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { setUpMap } from "./map";
 import { geoIpLocate } from "./geoIP"
 await setUpMap();
 
-let {_, geoLocation} = await setUpMap();
+let {map, geoLocation} = await setUpMap();
 showLocations("");
 
 // Search
@@ -17,4 +17,14 @@ geoLocation.on('geolocate', function (e) {
     showLocations("", e.coords.latitude, e.coords.longitude)
 })
 
-geoIpLocate()
+// Approximate location based on IP when geolocation is not available
+await navigator.permissions.query({ name: 'geolocation' }).then(async (value) => {
+        if (value.state != "granted") {
+            const geo_ip_bbox = await geoIpLocate();
+            console.log(geo_ip_bbox);
+            map.fitBounds(geo_ip_bbox)
+        }
+    }
+);
+
+
