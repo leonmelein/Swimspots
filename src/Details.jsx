770b58dcceec2f6deaa672b9ce18c959b9fa8456
Léon Melein <leon@thesociallions.nl>
2024-09-11 17:12:01 +0200

    Detail page tweaks

--- src/Details.jsx
+++ src/Details.jsx
@@ -4,8 +4,8 @@ import AmenityList from './AmenityList'
 import Header from './Header'
 import Map from './Map'
 
-import Icon from '@mdi/react';
-import { mdiBacteria, mdiPaperRoll } from '@mdi/js';
+// import Icon from '@mdi/react';
+// import { mdiArrowLeft, mdiArrowRight, mdiBacteria, mdiPaperRoll } from '@mdi/js';
 import { useLocation } from "react-router-dom";
 
 function Details(){
@@ -14,30 +14,49 @@ function Details(){
     return (
         <>
            <Header />
-            <div className="container details-container">
-                <h2>{state.name}</h2>
-                <p>Plaatsnaam</p>
-                <AmenityList amenities={state.amenities} />
-                <div>
-                    <h3 className='test-result-header'>Testresultaten</h3>
+            <div className="details-container">
+                <div className='location-headline'>
+                    <div className='container'>
+                            <nav>
+                                {/* <p className='backBtn'>
+                                    ← Terug
+                                </p> */}
+                            </nav>
+                            <h2 className='location-name'>{state.name}</h2>
+                            <AmenityList amenities={state.amenities} />
+                    </div>
+                </div>
+                <div className='location-details container'>
+                    <p>{state.description}</p>
+                </div>
+                <div className='location-tests container'>
+                    <h3 className='test-result-header'>Waterkwaliteit</h3>
                     <div className="test-result-container">
                         <div className="test-result">
-                            <Icon path={mdiBacteria} size='1lh' color='#60B332' />
+                            <p>Huidige kwaliteit</p>
+                            <p className='measurement'>Goed</p>
+                        </div>
+                        <div className="test-result">
+                            <p>Meerjarige kwaliteit</p>
+                            <p className='measurement'>★★★</p>
+                        </div>
+                        <div className="test-result">
+                            {/* <Icon path={mdiBacteria} size='1lh' color='#60B332' /> */}
                             <p>E.Coli</p>
                             <p className="measurement">{state.e_coli}</p>
                         </div>
                         <div className="test-result">
-                            <Icon path={mdiPaperRoll} size='1lh' color='grey' />
-                            <p>Intestinale enterococcen</p>
+                            {/* <Icon path={mdiPaperRoll} size='1lh' color='grey' /> */}
+                            <p>Enterococcen (IE)</p>
                             <p className="measurement">{state.int_ent}</p>
                         </div>
+                        <span className='lastUpdated'>Laatste meting: 7 september 2024</span>
+
                     </div>
                 </div>
-                <div>
-                    <h3>De locatie</h3>
-                    <p>{state.description}</p>
+                <div className='location-details container'>
+                    <h3>Kaart</h3>
                     <Map lat={state.lat} lng={state.lng}/>
-                    <em>Bron: <a href='https://zwemwater.nl'>zwemwater.nl</a></em>
                 </div>
             </div>
         </>
