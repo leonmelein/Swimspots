7bd805e5b563a63ba0084bcd50447e16f4f3463d
Léon Melein <leon@thesociallions.nl>
2024-09-14 22:36:37 +0200

    Web Share API

--- src/Details.jsx
+++ src/Details.jsx
@@ -6,12 +6,20 @@ import Map from './Map'
 
 import PropTypes from 'prop-types';
 import Icon from '@mdi/react';
-import { mdiAlert } from '@mdi/js';
+import { mdiAlert, mdiShare } from '@mdi/js';
 import { useLocation } from "react-router-dom";
 
 function Details(){
     const { state } = useLocation();
-    console.log(state.address);
+    const shareData = {
+        title: state.name,
+        text: state.description,
+        url: "https://swim.leonmelein.nl/location/"+state.id,
+    };
+
+    async function share(){
+        await navigator.share(shareData);
+    }
 
     return (
         <>
@@ -26,6 +34,12 @@ function Details(){
                 </div>
                 <div className='location-details container'>
                     <p>{state.description}</p>
+                    <button className='share'>
+                        <div className="share" onClick={share}>
+                            <Icon path={mdiShare} size='2rem' />
+                            <p>Deel deze locatie</p>
+                        </div>
+                    </button>
                 </div>
                 {/* <div className='location-warning'>
                     <Icon path={mdiAlert} size='1.2rem' color='black' />
@@ -58,6 +72,7 @@ function Details(){
 
                     </div>
                 </div>
+                
                 <div className='location-details container'>
                     <h3>Locatie</h3>
                     <p>{state.address}</p>
